ca26c38aa480f5e7daa30c9119885ec2bd4b7ed6
Christian Areas <me@areas.me>
2022-12-24 21:51:29 -0800

    Completed the GET /api/users and GET /api/users/:id routes.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,24 +1,60 @@
 // Import the Express router.
 const router = require("express").Router()
 
-// GET /api/users
+// Import the User model.
+const { User } = require("../../models")
+
+// GET /api/users.
 router.get("/", async (req, res) => {
   try {
-    console.log("GET /api/users!") // **
-    res.status(200).json("GET /api/users!") // **
+    const users = await User.findAll({
+      attributes: [
+        "user_id",
+        "first_name",
+        "last_name",
+        "email",
+      ],
+    })
+    res.status(200).json(users)
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
-// GET /api/users/favorites
-router.get("/favorites", async (req, res) => {
+// GET /api/users/:id.
+router.get("/:id", async (req, res) => {
   try {
-    console.log("GET /api/users/favorites!")
-    res.status(200).json("GET /api/users/favorites!")
+    const user = await User.findOne({
+      attributes: [
+        "user_id",
+        "first_name",
+        "last_name",
+        "email",
+      ],
+      where: {
+        "user_id": req.params.id,
+      },
+    })
+    res.status(200).json(user)
   } catch (err) {
     res.status(500).json(err)
   }
 })
+
+// POST /api/users/:id.
+
+
+// GET /api/users/favorites/:id
+router.get("/favorites/:id", async (req, res) => {
+  try {
+    console.log("GET /api/users/favorites/:id!")
+    res.status(200).json("GET /api/users/favorites/:id!")
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// POST /api/users/favorites/:id
+
 
 module.exports = router
