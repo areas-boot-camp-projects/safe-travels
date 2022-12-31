195e74bc55af9bced95dfed5217327e2e23e2cc5
Christian Areas <me@areas.me>
2022-12-31 17:52:21 -0500

    Started to develop GET /api/covid/:country/:state route.

--- controllers/api/covidRoutes.js
+++ controllers/api/covidRoutes.js
@@ -7,16 +7,27 @@ const axios = require("axios")
 // COVID-19 API base URL.
 const baseUrl = "https://api.covid19api.com"
 
-// GET /api/covid/:country/:state
+// GET /api/covid/:country/:state route (get covid data for state and city)
 router.get("/:country/:state", async (req, res) => {
   try {
+    // Save the path parameters.
     const country = req.params.country
     const state = req.params.state
-    const fromDate = "2022-12-01T00:00:00Z"
-    const toDate = "2022-12-01T01:00:00Z"
-    const requestUrl = `${baseUrl}/country/${country}?province=${state}&from=${fromDate}&to=${toDate}`
+    // Save the query parameters.
+    const city = req.query.city
+    const from = req.query.from
+    const to = req.query.to
+    // Create the COVID 19 API request URL and call it.
+    const requestUrl = `${baseUrl}/country/${country}?province=${state}&from=${from}&to=${to}`
     const result = await axios.get(requestUrl)
-    res.status(200).json(result.data)
+    // Filter results by city, or send it all.
+    let cityData
+    if (req.query.city) {
+      cityData = result.data.filter(cityData => cityData.City === city)
+    } else {
+      cityData = result.data
+    }
+    res.status(200).json(cityData)
   } catch (err) {
     res.status(500).json(err)
   }
