d69eedeb587d657ff93b29614e250076b56e85c6
Christian Areas <me@areas.me>
2023-01-03 02:37:07 -0500

    Finished covid route and refactored user routes.

--- controllers/api/covidRoutes.js
+++ controllers/api/covidRoutes.js
@@ -1,33 +1,33 @@
 // Import the Express router.
 const router = require("express").Router()
 
-// Import axios.
+// Import the Axios library.
 const axios = require("axios")
 
-// COVID-19 API base URL.
+// Declare the COVID-19 API base URL.
 const baseUrl = "https://api.covid19api.com"
 
-// GET /api/covid/:country/:state route (get covid data for state and city)
+// Declare the GET /api/covid/:country/:state route ().
 router.get("/:country/:state", async (req, res) => {
   try {
-    // Save the path parameters.
+    // Declare the region and date parameters.
     const country = req.params.country
     const state = req.params.state
-    // Save the query parameters.
     const city = req.query.city
-    const from = req.query.from
-    const to = req.query.to
-    // Create the COVID 19 API request URL and call it.
+    const from = `${req.query.date}T00:00:00Z`
+    const to = `${req.query.date}T23:59:59Z`
+    // Declare the COVID 19 API request URL and call it (see also: /country/:country).
     const requestUrl = `${baseUrl}/country/${country}?province=${state}&from=${from}&to=${to}`
     const result = await axios.get(requestUrl)
-    // Filter results by city, or send it all.
-    let cityData
+    // Return the city’s, or all cities’, COVID data.
+    let covidData
     if (req.query.city) {
-      cityData = result.data.filter(cityData => cityData.City === city)
-    } else {
-      cityData = result.data
+      covidData = result.data.filter(covidData => covidData.City === city)
     }
-    res.status(200).json(cityData)
+    else {
+      covidData = result.data
+    }
+    res.status(200).json(covidData)
   } catch (err) {
     res.status(500).json(err)
   }
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,14 +1,14 @@
 // Import the Express router.
 const router = require("express").Router()
 
+const { default: test } = require("node:test")
 // Import Sequelize operators.
-const { Op } = require("sequelize")
+const { Op, Sequelize } = require("sequelize")
 
-// Import the User model.
+// Import the User and UserFavorite model.
 const { User, UserFavorite } = require("../../models")
 
-// GET /api/users route (get all users).
-// Todo: Investigate if it would be good to create docs using swagger-jsdoc (code-first approach), or swagger-codegen or openapi-generator (design-first approach). **
+// Declare the GET /api/users route (get all users).
 router.get("/", async (req, res) => {
   try {
     const users = await User.findAll({
@@ -21,12 +21,11 @@ router.get("/", async (req, res) => {
     })
     res.status(200).json(users)
   } catch (err) {
-  // todo/nice-to-have: Add if/else statements to catch other errors, such as 400 and 404. **
     res.status(500).json(err)
   }
 })
 
-// GET /api/users/:id route (get a user).
+// Declare GET /api/users/:id route (get a user).
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findOne({
@@ -46,7 +45,7 @@ router.get("/:id", async (req, res) => {
   }
 })
 
-// POST /api/users (add a user).
+// Declare POST /api/users (add a user).
 router.post("/", async (req, res) => {
   try {
     const user = await User.create(req.body)
@@ -56,10 +55,11 @@ router.post("/", async (req, res) => {
   }
 })
 
-// GET /api/users/:id/favorites (get a user’s favorites)
+// Declare GET /api/users/:id/favorites (get a user’s favorites)
 router.get("/:id/favorites", async (req, res) => {
   try {
-    const userFavorites = await UserFavorite.findAll({
+    // ** if possible, move this outside the route and make it available to all the favorites routes.
+    const allUserFavoritesObjects = await UserFavorite.findAll({
       attributes: [
         "favorite",
       ],
@@ -67,42 +67,69 @@ router.get("/:id/favorites", async (req, res) => {
         "user_id": req.params.id,
       },
     })
-    res.status(200).json(userFavorites)
+    const allUserFavoritesArray = allUserFavoritesObjects.map(favorite => favorite.favorite)    
+    res.status(200).json(allUserFavoritesArray)
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
-// POST /api/users/:id/favorites (add a user’s favorites)
+// Declare POST /api/users/:id/favorites (add a user’s favorites)
 router.post("/:id/favorites", async (req, res) => {
   try {
-    const favorites = []
+    // 
+    const newUserFavoritesObjects = []
     req.body.forEach((favorite) => {
-      favorites.push({
+      newUserFavoritesObjects.push({
         user_id: req.params.id,
         favorite: favorite,
       })
     })
-    const userFavorites = await UserFavorite.bulkCreate(favorites)
-    res.status(200).json(userFavorites)
+    await UserFavorite.bulkCreate(newUserFavoritesObjects)
+    // ** if possible, move this outside the route and make it available to all the favorites routes.
+    const allUserFavoritesObjects = await UserFavorite.findAll({
+      attributes: [
+        "favorite",
+      ],
+      where: {
+        "user_id": req.params.id,
+      },
+    })
+    const allUserFavoritesArray = allUserFavoritesObjects.map(favorite => favorite.favorite)    
+    res.status(200).json(allUserFavoritesArray)
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
-// DELELTE /api/users/:id/favorites (delete a user’s favorites)
+// Declare DELELTE /api/users/:id/favorites (delete a user’s favorites)
 router.delete("/:id/favorites", async (req, res) => {
   try {
-    const userFavorites = await UserFavorite.destroy({
+    await UserFavorite.destroy({
       where: {
         "user_id": req.params.id,
         "favorite": { [Op.in]: req.body },
       },
     })
-    res.status(200).json(userFavorites)
+    // ** if possible, move this outside the route and make it available to all the favorites routes.
+    const allUserFavoritesObjects = await UserFavorite.findAll({
+      attributes: [
+        "favorite",
+      ],
+      where: {
+        "user_id": req.params.id,
+      },
+    })
+    const allUserFavoritesArray = allUserFavoritesObjects.map(favorite => favorite.favorite)    
+    res.status(200).json(allUserFavoritesArray)
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
 module.exports = router
+
+// Questions/todos/nice-to-haves:
+// - For POST /api/users/, do we need to return the password?
+// - Investigate if it would be good to create docs using swagger-jsdoc (code-first approach), or swagger-codegen or openapi-generator (design-first approach). **
+// - Add if/else statements to catch other errors, such as 400 and 404 (not just 500).
