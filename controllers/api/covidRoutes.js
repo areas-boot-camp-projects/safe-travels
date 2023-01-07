c8733f617c7721de623f60a653325ff2483e7042
Christian Areas <me@areas.me>
2023-01-06 19:32:09 -0500

    Updated to a different endpoint that performs a tiny bit better.

--- controllers/api/covidRoutes.js
+++ controllers/api/covidRoutes.js
@@ -7,21 +7,24 @@ const axios = require("axios")
 // Declare the COVID-19 API base URL.
 const baseUrl = "https://api.covid19api.com"
 
-// Declare the GET /api/covid/:country/:state route ().
-covidRouter.get("/:country/:state", async (req, res) => {
+// Declare the GET /api/covid/:country/:state/:city/:type route (get cases per city).
+covidRouter.get("/:country/:state/:city/:type", async (req, res) => {
   try {
     // Declare the region and date parameters.
+    console.log(req.params)
     const country = req.params.country
     const state = req.params.state
-    const city = req.query.city
+    const city = req.params.city
+    const type = req.params.type
     const from = `${req.query.date}T00:00:00Z`
     const to = `${req.query.date}T23:59:59Z`
-    // Declare the COVID 19 API request URL and call it (see also: /country/:country).
-    const requestUrl = `${baseUrl}/country/${country}?province=${state}&from=${from}&to=${to}`
+    // Declare the COVID 19 API request URL and call it (see also: /country/:country/status/:status).
+    const requestUrl = `${baseUrl}/country/${country}/status/${type}?province=${state}&from=${from}&to=${to}`
+    console.log(requestUrl) // **
     const result = await axios.get(requestUrl)
     // Return the city’s, or all cities’, COVID data.
     let covidData
-    if (req.query.city) {
+    if (req.params.city) {
       covidData = result.data.filter(covidData => covidData.City === city)
     }
     else {
