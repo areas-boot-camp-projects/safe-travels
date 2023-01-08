541be9e05d6d8f918137e0ef5e967b05e621fcc9
Christian Areas <me@areas.me>
2023-01-08 03:25:53 -0500

    Added async/await and try/catch to all page routes.

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -1,13 +1,11 @@
-// todo: Add async/await and try/catch to all routes.
-
-// Import the Express router.
+// Import the Express router and Sequelize operators.
 const pageRouter = require("express").Router()
-
-// Import the User and UserFavorite models.
-const { User, UserFavorite } = require("../../models")
 
 // Import JWT.
 const jwt = require("jsonwebtoken")
+
+// Import the getUser() util function.
+const getUser = require("../../utils/getUser")
 
 // Get a user’s token from their cookie.
 function getToken(cookie) {
@@ -18,82 +16,52 @@ function getToken(cookie) {
   return token
 }
 
-// Get a user’s details and favorites and return them as an object. ** todo: move this out so it’s available to other files.
-async function getUser(userId) {
-  try {
-    // Query the database.
-    let user = await User.findOne({
-      attributes: [
-        "user_id",
-        "first_name",
-        "last_name",
-        "email",
-      ],
-      where: {
-        "user_id": userId,
-      },
-      include: [{
-        model: UserFavorite,
-        required: false,
-        attributes: [
-          "favorite_city",
-          "favorite_state",
-        ],
-        where: {
-          "user_id": userId,
-        },
-      }],
-    })
-    // Convert the output to a plain JavaScript object.
-    user = user.toJSON()
-    // Rename “UserFavorites” to “favorites”.
-    user = {
-      ...user,
-      favorites: user.UserFavorites,
-    }
-    delete user.UserFavorites
-    // Return the new object.
-    return user
-  } catch (err) {
-    throw err
-  }
-}
-
-// Declare the GET / route (get the home page).
+// Declare the GET / route (render the home page).
 pageRouter.get("/", async (req, res) => {
-  const token = await getToken(req.headers.cookie)
-  if (token) {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
-    const user = await getUser(decodedToken.user_id)
-    console.log(user) // **
-    res.render("home", {
-      token: token,
-      user: user,
-    })
-  } else {
-    res.render("home", {
-      token: token,
-    })
+  try {
+    const token = await getToken(req.headers.cookie)
+    if (token) {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+      const user = await getUser(decodedToken.user_id)
+      res.render("home", {
+        token: token,
+        user: user,
+      })
+    } else {
+      res.render("home", {
+        token: token,
+      })
+    }
+  } catch (err) {
+    res.status(500).json(err)
   }
 })
 
-// Declare the GET /sign-up route (get the home page).
-pageRouter.get("/sign-up", (req, res) => {
-  const token = getToken(req.headers.cookie)
-  if (!token) {
-    res.render("sign-up", { token: token })
-  } else if (token) {
-    res.redirect("/")
+// Declare the GET /sign-up route (render the sign up page).
+pageRouter.get("/sign-up", async (req, res) => {
+  try {
+    const token = await getToken(req.headers.cookie)
+    if (!token) {
+      res.render("sign-up", { token: token })
+    } else if (token) {
+      res.redirect("/")
+    }
+  } catch (err) {
+    res.status(500).json(err)
   }
 })
 
-// Declare the GET /sign-in route (get the home page).
-pageRouter.get("/sign-in", (req, res) => {
-  const token = getToken(req.headers.cookie)
-  if (!token) {
-    res.render("sign-in", { token: token })
-  } else if (token) {
-    res.redirect("/")
+// Declare the GET /sign-in route (render the sign in page).
+pageRouter.get("/sign-in", async (req, res) => {
+  try {
+    const token = getToken(req.headers.cookie)
+    if (!token) {
+      res.render("sign-in", { token: token })
+    } else if (token) {
+      res.redirect("/")
+    }
+  } catch (err) {
+    res.status(500).json(err)
   }
 })
 
